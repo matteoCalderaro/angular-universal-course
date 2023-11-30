6c3bc34a04663525ce71bb1aa3504942ad1a81c1
Matteo Calderaro <Matteocalderaro@gmail.com>
2023-11-30 13:46:36 +0000

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,40 +1,36 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import {MatMenuModule} from '@angular/material/menu';
-import {MatButtonModule} from '@angular/material/button'
-import {MatIconModule} from '@angular/material/icon';
-import {MatCardModule} from '@angular/material/card';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
 import { HomeComponent } from './home/home.component';
-import {MatTabsModule} from '@angular/material/tabs';
+import { MatTabsModule } from '@angular/material/tabs';
 import { CoursesCardListComponent } from './courses-card-list/courses-card-list.component';
-import {CourseComponent} from "./course/course.component";
-import { MatDatepickerModule } from "@angular/material/datepicker";
-import { MatDialogModule } from "@angular/material/dialog";
-import { MatInputModule } from "@angular/material/input";
-import { MatListModule } from "@angular/material/list";
-import { MatPaginatorModule } from "@angular/material/paginator";
-import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
-import { MatSelectModule } from "@angular/material/select";
-import { MatSidenavModule } from "@angular/material/sidenav";
-import { MatSortModule } from "@angular/material/sort";
-import { MatTableModule } from "@angular/material/table";
-import { MatToolbarModule } from "@angular/material/toolbar";
-import {CoursesService} from "./services/courses.service";
-import {CourseResolver} from "./services/course.resolver";
+import { CourseComponent } from './course/course.component';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { CoursesService } from './services/courses.service';
+import { CourseResolver } from './services/course.resolver';
 import { CourseDialogComponent } from './course-dialog/course-dialog.component';
-import {ReactiveFormsModule} from "@angular/forms";
-import { HttpClientModule} from '@angular/common/http';
-import {AboutComponent} from './about/about.component';
-import {AppShellRenderDirective} from "./directives/app-shell-render.directive";
-import {AppShellNoRenderDirective} from "./directives/app-shell-norender.directive";
-
-
-
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { AboutComponent } from './about/about.component';
+// import { AppShellRenderDirective } from './directives/app-shell-render.directive';
+// import { AppShellNoRenderDirective } from './directives/app-shell-norender.directive';
 
 @NgModule({
   declarations: [
@@ -44,11 +40,12 @@ import {AppShellNoRenderDirective} from "./directives/app-shell-norender.directi
     CoursesCardListComponent,
     CourseDialogComponent,
     AboutComponent,
-    AppShellRenderDirective,
-    AppShellNoRenderDirective
+    // AppShellRenderDirective,
+    // AppShellNoRenderDirective,
   ],
   imports: [
-    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    // BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    BrowserModule,
     //BrowserTransferStateModule,
     BrowserAnimationsModule,
     MatMenuModule,
@@ -69,13 +66,9 @@ import {AppShellNoRenderDirective} from "./directives/app-shell-norender.directi
     MatSelectModule,
     MatDatepickerModule,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
   ],
-  providers: [
-    CoursesService,
-    CourseResolver
-  ],
-  bootstrap: [AppComponent]
+  providers: [CoursesService, CourseResolver],
+  bootstrap: [AppComponent],
 })
-export class AppModule {
-}
+export class AppModule {}
